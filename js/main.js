4b4b05c8fef3a126e86b3533dabe081a4af4853d
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-02 12:36:47 +0100

    form

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.querySelector('button').onclick = function(){
+    document.querySelector('#name').textContent=
+document.querySelector('#input-name').value;
+}
+document.querySelector('button').onclick = function(){
+    document.querySelector('#surname').textContent=
+document.querySelector('#lastname').value;
+}
+document.querySelector('button').onclick = function(){
+    document.querySelector('#phone').textContent=
+document.querySelector('#input-phone').value;
+}
