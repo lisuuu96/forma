d7aa1d0108d98f99120c0c56a4b65d08d7c17589
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-03 18:00:16 +0100

    change

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 document.querySelector('button').onclick = function(){
     document.querySelector('#name').textContent=
 document.querySelector('#input-name').value;
-}
-document.querySelector('button').onclick = function(){
-    document.querySelector('#surname').textContent=
 document.querySelector('#lastname').value;
-}
-document.querySelector('button').onclick = function(){
-    document.querySelector('#phone').textContent=
 document.querySelector('#input-phone').value;
 }
+
