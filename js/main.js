9206293a035a9267d1e06302acd7be296dcc3b18
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-05 09:21:37 +0100

    cor

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 document.querySelector('button').onclick = function(){
-    document.querySelector('#name').textContent=
-document.querySelector('#input-name').value;
-document.querySelector('#lastname').value;
-document.querySelector('#input-phone').value;
+    document.querySelector('#name').textContent = document.querySelector('#input-name').value;
+    document.querySelector('#surname').textContent = document.querySelector('#lastname').value;
+    document.querySelector('#phone').textContent = document.querySelector('#input-phone').value;
 }
 
